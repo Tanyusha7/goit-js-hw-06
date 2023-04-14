4a4df63cd383fc62f59f155f364ca9c9c6375a73
Tanyusha7 <swanonlake@gmail.com>
2023-04-15 00:51:41 +0200

    update task-02.js, task-03.js

---
+++ src/js/task-01.js
@@ -0,0 +1,24 @@
+const ulEl = document.querySelector("ul#categories");
+console.log(`Number of categories: ${ulEl.children.length}`);
+console.log(ulEl.children);
+
+ulEl.childNodes.forEach((category) => category);
+console.log(ulEl.childNodes(category));
+
+// console.log(ulEl.textContent);
+
+// console.log(ulEl.parentNode);
+// console.log(ulEl.childNodes);
+// console.log(ulEl.firstChild);
+// console.log(ulEl.firstElementChild);
+// console.log(ulEl.lastElementChild);
+// console.log(ulEl.previousSibling);
+// console.log(ulEl.nextSibling);
+
+// const liEl = document.querySelectorAll("li");
+// console.log(liEl);
+
+// const liItemEl = document.querySelectorAll(".item");
+// console.log(liItemEl);
+
+// console.log(liItemEl[0].textContent);
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,33 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const list = document.querySelector(".gallery");
+
+list.style.paddingRight = "40px";
+list.style.display = "flex";
+list.style.justifyContent = "center";
+list.style.alignItems = "center";
+list.style.listStyle = "none";
+list.style.gap = "24px";
+
+const imageElem = images.map((image) => {
+  const liElemImg = `<li>
+  <img src = ${image.url} alt = ${image.alt} width = 300 />
+  </li>`;
+
+  // console.log(liElemImg);
+  return liElemImg;
+});
+list.insertAdjacentHTML("beforeend", imageElem);
