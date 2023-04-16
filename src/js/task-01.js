683f612c66fd2d68fa60fd78707dbaa31c1f6367
Tanyusha7 <swanonlake@gmail.com>
2023-04-16 22:54:03 +0200

    update task-01.js, task-04.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,24 +1,10 @@
-const ulEl = document.querySelector("ul#categories");
+const ulEl = document.querySelector("#categories");
+
 console.log(`Number of categories: ${ulEl.children.length}`);
-console.log(ulEl.children);
 
-ulEl.childNodes.forEach((category) => category);
-console.log(ulEl.childNodes(category));
+const liEl = document.querySelectorAll(".item");
 
-// console.log(ulEl.textContent);
-
-// console.log(ulEl.parentNode);
-// console.log(ulEl.childNodes);
-// console.log(ulEl.firstChild);
-// console.log(ulEl.firstElementChild);
-// console.log(ulEl.lastElementChild);
-// console.log(ulEl.previousSibling);
-// console.log(ulEl.nextSibling);
-
-// const liEl = document.querySelectorAll("li");
-// console.log(liEl);
-
-// const liItemEl = document.querySelectorAll(".item");
-// console.log(liItemEl);
-
-// console.log(liItemEl[0].textContent);
+liEl.forEach((name) => {
+  console.log(`Category: ${name.firstElementChild.textContent}`);
+  console.log(`Elements: ${name.lastElementChild.children.length}`);
+});
