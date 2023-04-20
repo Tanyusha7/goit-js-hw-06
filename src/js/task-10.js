f0b2a34921c970b242983ca8bed2da9ad0c9d994
Tanyusha7 <swanonlake@gmail.com>
2023-04-21 01:31:36 +0200

    update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -5,35 +5,61 @@ function getRandomHexColor() {
 }
 
 const controlsEl = document.querySelector("#controls");
-console.dir(controlsEl);
-console.log(controlsEl.firstElementChild);
-console.log(controlsEl.firstElementChild.nextElementSibling);
-console.log(controlsEl.lastElementChild);
+// console.dir(controlsEl);
+// console.log(controlsEl.firstElementChild.getAttribute("max"));
+// console.log(controlsEl.firstElementChild.getAttribute("min"));
 
 const input = document.querySelector("input");
-console.log(input);
 
 const createBtn = document.querySelector("[data-create]");
-console.log(createBtn);
-
+// console.log(createBtn.previousElementSibling);
 const destroyBtn = document.querySelector("[data-destroy]");
-console.log(destroyBtn);
 
 const boxes = document.querySelector("#boxes");
-console.dir(boxes);
+boxes.style.margin = "10px";
+boxes.style.display = "flex";
+boxes.style.flexWrap = "wrap";
+boxes.style.gap = "10px";
+// console.dir(boxes);
 
-createBtn.addEventListener("click", onCreateBtn);
-destroyBtn.addEventListener("click", onDestroyBtn);
+createBtn.addEventListener("click", onBtnCreate);
+destroyBtn.addEventListener("click", destroyBoxes);
 
-function createBoxes(numbers) {
-  boxes.style.width = "30px";
-  boxes.style.height = "30px";
+const elementsBoxes = [];
+
+function createBoxes(amount) {
+  amount = createBtn.previousElementSibling.value;
+  for (let i = 1; i <= amount; i += 1) {
+    console.log(i);
+    console.log(amount);
+    const divEl = document.createElement("div");
+    divEl.style.textAlign = "center";
+    divEl.style.width = "30px";
+    divEl.style.height = "30px";
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.textContent = i;
+
+    // console.log(divEl.style.width`&{+=10}px`);
+
+    console.dir(divEl);
+    elementsBoxes.push(divEl);
+    boxes.append(...elementsBoxes);
+  }
 }
 
-function onCreateBtn() {
+function onBtnCreate() {
+  input.addEventListener("input", onInput());
+  createBoxes();
   console.log("Stvorennya");
 }
 
-function onDestroyBtn() {
+function destroyBoxes() {
   console.log("vudalennya");
+
+  boxes.remove();
+}
+
+function onInput() {
+  console.log("chislo");
+  // createBoxes(number);
 }
