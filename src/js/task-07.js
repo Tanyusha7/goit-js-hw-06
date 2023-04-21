f32b0b83fdb7ec07da34243a65805dfd051733bb
Tanyusha7 <swanonlake@gmail.com>
2023-04-21 23:04:22 +0200

    delete files (src)

--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputEl = document.querySelector("#font-size-control");
-
-const inputText = document.querySelector("#text");
-
-inputEl.addEventListener("input", onInputEl);
-
-function onInputEl(event) {
-  inputText.style.fontSize = `${event.currentTarget.value}px`;
-}
