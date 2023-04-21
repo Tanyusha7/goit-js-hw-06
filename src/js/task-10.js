2cefba1608a70ab9774371bd4254b6f8976c56d9
Tanyusha7 <swanonlake@gmail.com>
2023-04-21 09:28:19 +0200

    update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -10,9 +10,7 @@ const controlsEl = document.querySelector("#controls");
 // console.log(controlsEl.firstElementChild.getAttribute("min"));
 
 const input = document.querySelector("input");
-
 const createBtn = document.querySelector("[data-create]");
-// console.log(createBtn.previousElementSibling);
 const destroyBtn = document.querySelector("[data-destroy]");
 
 const boxes = document.querySelector("#boxes");
@@ -20,7 +18,6 @@ boxes.style.margin = "10px";
 boxes.style.display = "flex";
 boxes.style.flexWrap = "wrap";
 boxes.style.gap = "10px";
-// console.dir(boxes);
 
 createBtn.addEventListener("click", onBtnCreate);
 destroyBtn.addEventListener("click", destroyBoxes);
@@ -30,18 +27,15 @@ const elementsBoxes = [];
 function createBoxes(amount) {
   amount = createBtn.previousElementSibling.value;
   for (let i = 1; i <= amount; i += 1) {
-    console.log(i);
-    console.log(amount);
+    // console.log(i);
+    // console.log(amount);
     const divEl = document.createElement("div");
     divEl.style.textAlign = "center";
     divEl.style.width = "30px";
-    divEl.style.height = "30px";
+    divEl.style.height += "30px";
     divEl.style.backgroundColor = getRandomHexColor();
     divEl.textContent = i;
 
-    // console.log(divEl.style.width`&{+=10}px`);
-
-    console.dir(divEl);
     elementsBoxes.push(divEl);
     boxes.append(...elementsBoxes);
   }
@@ -50,16 +44,10 @@ function createBoxes(amount) {
 function onBtnCreate() {
   input.addEventListener("input", onInput());
   createBoxes();
-  console.log("Stvorennya");
 }
 
 function destroyBoxes() {
-  console.log("vudalennya");
-
   boxes.remove();
 }
 
-function onInput() {
-  console.log("chislo");
-  // createBoxes(number);
-}
+function onInput() {}
