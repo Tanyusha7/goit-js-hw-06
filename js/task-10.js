c95b08013f454b0bd5214ecf92a152e48148f6d6
Tanyusha7 <swanonlake@gmail.com>
2023-04-22 22:00:09 +0200

    update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,14 +4,8 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const controlsEl = document.querySelector("#controls");
-// console.dir(controlsEl);
-// console.log(controlsEl.firstElementChild.getAttribute("max"));
-// console.log(controlsEl.firstElementChild.getAttribute("min"));
-
-const input = document.querySelector("input");
 const createBtn = document.querySelector("[data-create]");
-const destroyBtn = document.querySelector("[data-destroy]");
+const destroyBoxes = document.querySelector("[data-destroy]");
 
 const boxes = document.querySelector("#boxes");
 boxes.style.margin = "10px";
@@ -19,36 +13,30 @@ boxes.style.display = "flex";
 boxes.style.flexWrap = "wrap";
 boxes.style.gap = "10px";
 
-createBtn.addEventListener("click", onBtnCreate);
-destroyBtn.addEventListener("click", destroyBoxes);
-
-const elementsBoxes = [];
-
 function createBoxes(amount) {
-  amount = createBtn.previousElementSibling.value;
   for (let i = 1; i <= amount; i += 1) {
     console.log(i);
     console.log(amount);
-    const divEl = document.createElement("div");
+    let divEl = document.createElement("div");
     divEl.style.textAlign = "center";
-    divEl.style.width = `${30 + 10}px`;
-    divEl.style.height = `${30 + i}px`;
+    divEl.style.width = 30 + 10 * i + "px";
+    divEl.style.height = 30 + 10 * i + "px";
     divEl.style.backgroundColor = getRandomHexColor();
     divEl.textContent = i;
-    divEl.style.width = `${divEl.style.width + 10}px`;
 
-    elementsBoxes.push(divEl);
-    boxes.append(...elementsBoxes);
+    console.log(divEl);
+    boxes.append(divEl);
   }
 }
 
-function onBtnCreate() {
-  input.addEventListener("input", onInput());
-  createBoxes();
-}
+createBtn.addEventListener("click", function () {
+  let amount = createBtn.previousElementSibling.value;
+  if (amount > 0) {
+    createBoxes(amount);
+  }
+});
 
-function destroyBoxes() {
-  boxes.remove();
-}
-
-function onInput() {}
+destroyBoxes.addEventListener("click", function () {
+  boxes.innerHTML = "";
+  createBtn.previousElementSibling.value = "";
+});
